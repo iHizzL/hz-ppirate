a0167f91d8c26cfe9b1e4cca20d63ffc32a36314
HizzL <nicolaibakken@hotmail.com>
2024-10-02 23:01:29 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,21 +2,18 @@ fx_version 'adamant'
 game 'gta5'
 lua54 'yes'
 
+author 'HizzL'
+description 'A dynamic package spawning system inspired by porch pirates. Spawn mysterious packages across the map for players to find, pick up, and open for random loot.'
+
 client_scripts {
-    'client/*.lua',
-    '@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-    '@PolyZone/EntityZone.lua',
-    '@PolyZone/CircleZone.lua',
-    '@PolyZone/ComboZone.lua',
+    'client.lua'
 }
 
 server_scripts {
-    'server/*.lua',
+    'server.lua'
 }
 
 shared_scripts {
     '@ox_lib/init.lua',
-	'shared/*.lua',
-	'config.lua',
+    'config.lua'
 }
